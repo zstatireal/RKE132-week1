c22a672d27ab933c44c0b5008b79e6c7bb14d610
zstati <eerik3d@pm.me>
2026-02-19 17:46:50 +0200

    Lisan week1 asjad

---
+++ main.py
@@ -0,0 +1,13 @@
+# Alusta
+#
+# Lõpeta
+
+# pseudokood - nt retsept, mida oskab lugeda iga inimene ja see kirjeldab programmi tööpõhimõtet
+# vooskeem - planeerimine ja õppimiseks
+# 1. Ovaal algus ja lõpp
+# 2.1 ristkülik tegevus või arvutus
+# 2.2 rööpkülik I või O
+# 2.3 teemant boolean bäitab nii jah kui ei
+# ülevalt alla V vasakult paremale
+# noole suunad
+# diagrams.net E draw.io
